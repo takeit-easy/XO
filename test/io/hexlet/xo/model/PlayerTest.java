8049b9a81e891e25f48864f0aa2ca33aa6fa55c4
takeit-easy <su-su.i@mail.ru>
2019-05-17 12:52:17 +0300

    PlayerTest created

---
+++ test/io/hexlet/xo/model/PlayerTest.java
@@ -0,0 +1,31 @@
+package io.hexlet.xo.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Test
+    public void getName() {
+        final String inputValue = "Slava";
+        final String expectedValue = inputValue;
+
+        final Player player = new Player(inputValue, null);
+
+        final String actualValue = player.getName();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void getFigure() {
+        final Figure inputValue = Figure.X;
+        final Figure expectedValue = inputValue;
+
+        final Player player = new Player(null, inputValue);
+
+        final Figure actualValue = player.getFigure();
+
+        assertEquals(expectedValue, actualValue);}
+}
