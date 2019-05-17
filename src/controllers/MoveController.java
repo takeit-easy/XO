43803eca3a95fd0571c255d8149dc3f985abe3f4
takeit-easy <su-su.i@mail.ru>
2019-05-17 16:34:33 +0300

    Initial MoveController

---
+++ src/controllers/MoveController.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import com.sun.org.apache.xml.internal.security.exceptions.AlgorithmAlreadyRegisteredException;
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Figure;
+import io.hexlet.xo.model.exceptions.AlreadyOccupiedException;
+import io.hexlet.xo.model.exceptions.InvalidPointException;
+
+import java.awt.*;
+
+public class MoveController {
+
+    public void applyFigure(final Field field,
+                            final Point point,
+                            final Figure figure) throws InvalidPointException, AlreadyOccupiedException{
+        if (field.getFigure(point) != null) {
+            throw new AlreadyOccupiedException();
+        }
+        field.setFigure(point, figure);
+    }
+
+}
