4268fd9e4d3a94455afa84cca1bb85ad8fa91e0f
takeit-easy <su-su.i@mail.ru>
2019-05-19 12:01:10 +0300

    fixes for WinnerController(were fixed the issues,that were discovered during unit testing)

--- src/controllers/WinnerController.java
+++ src/controllers/WinnerController.java
@@ -10,18 +10,21 @@ public class WinnerController {
 
     public Figure getWinner(final Field field) {
         try {
-            for (int i = 0; i < 3; i++) {
-                if (check(field, new Point(i, 0), p -> new Point(p.x+1,p.y)))
+            for (int i = 0; i < 3; i++)
+                if (check(field, new Point(i, 0), p -> new Point(p.x, p.y + 1)))
                     return field.getFigure(new Point(i, 0));
-                if (check(field, new Point(0, i), p -> new Point(p.x,p.y+1)))
-                    return field.getFigure(new Point(i, 0));
-            }
-            if (check(field, new Point(0, 0), p -> new Point(p.x+1,p.y+1)))
-                return field.getFigure(new Point(0, 0));
-            if (check(field, new Point(0, 2), p -> new Point(p.x-1,p.y-1)))
-                return field.getFigure(new Point(0, 2));
 
-        } catch (InvalidPointException e) {
+            for (int i = 0; i < 3; i++)
+                if (check(field, new Point(0, i), p -> new Point(p.x + 1, p.y)))
+                    return field.getFigure(new Point(0, i));
+
+            if (check(field, new Point(0, 0), p -> new Point(p.x + 1, p.y + 1)))
+                return field.getFigure(new Point(0, 0));
+
+            if (check(field, new Point(0, 2), p -> new Point(p.x + 1, p.y - 1)))
+                return field.getFigure(new Point(1, 1));
+
+        } catch (final InvalidPointException e) {
             e.printStackTrace();
         }
         return null;
@@ -36,14 +39,17 @@ public class WinnerController {
 
         try {
             currentFigure = field.getFigure(currentPoint);
-            nextFigure = field.getFigure(currentPoint);
+
+            if (currentFigure == null)
+                return false;
+
+            nextFigure = field.getFigure(nextPoint);
         } catch (InvalidPointException e) {
             return true;
         }
 
-        if (currentFigure == null) return false;
-
-        if (currentFigure != nextFigure) return false;
+        if (currentFigure != nextFigure)
+            return false;
 
         return check(field, nextPoint, pointGenerator);
     }
